d41c0ca4fe1f1298ff0cdf01baf210ae95e8e1d6
Shaggy124 <angelzb80@gmail.com>
2024-06-29 18:20:32 -0400

    hola

--- misProyectos - v1/alumnos/views.py
+++ misProyectos - v1/alumnos/views.py
@@ -22,3 +22,10 @@ def crud(request):
     alumnos= Alumno.objects.all()
     context={"alumnos":alumnos}
     return render(request, 'alumnos/alumnos_list.html', context)
+
+def alumno_add(request):
+    # Lógica para agregar un alumno
+    if request.methos is not "POST":
+        generos=Genero.objects.all()
+        context={'generos':generos}
+        return render(request, 'alumnos/alumno_add.html',context)
