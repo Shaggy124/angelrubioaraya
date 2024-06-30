da1ba0e6c2f609f02bdfc67edbe394aad9dfa5e2
Shaggy124 <angelzb80@gmail.com>
2024-06-30 15:15:03 -0400

    remover otros proyectos del repositorio

--- misProyectos - v1/alumnos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Alumno,Genero,Ramo,Seccion,AlumnoSeccion
-
-# Register your models here.
-admin.site.register(Genero)
-admin.site.register(Alumno)
-admin.site.register(Ramo)
-admin.site.register(Seccion)
-admin.site.register(AlumnoSeccion)
