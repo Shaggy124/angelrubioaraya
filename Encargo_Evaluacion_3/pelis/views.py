c515e63d57b2b859c75b2e9723c6df4097ac4dfe
Shaggy124 <angelzb80@gmail.com>
2024-07-01 21:56:03 -0400

    nota agregada en views

--- Encargo_Evaluacion_3/pelis/views.py
+++ Encargo_Evaluacion_3/pelis/views.py
@@ -7,7 +7,7 @@ def index(request):
     pelis= Peliculas.objects.all()
     context={"peliculas":pelis}
     return render(request, 'pelis/index.html', context)
-
+#Arreglar error que evita que se vean las 2 listas a la vez
 def index(request):
     canci= Cancion.objects.all()
     context={"cancion":canci}
